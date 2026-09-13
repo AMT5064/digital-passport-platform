ded0818758e55183ea7698acf4ee0b3b61760f49
Abhishek Mani Tripathi <abhishek.mt@kestoneglobal.com>
2026-09-13 18:54:14 +0530

    fix: Use require for bcrypt to bypass type checking

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,8 +2,8 @@ import { NextAuthOptions } from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import { prisma } from './prisma'
-// @ts-ignore
-import bcrypt from 'bcrypt'
+
+const bcrypt = require('bcrypt')
 
 declare module 'next-auth' {
   interface Session {
